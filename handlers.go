64b457698af0afba311247b0588468b2d4d7961b
Jon Hadfield <jon@lessknown.co.uk>
2025-07-26 19:46:52 +0100

    fix display

--- handlers.go
+++ handlers.go
@@ -40,42 +40,47 @@ func (h *Handler) HandleRoot(c echo.Context) error {
 
 // buildResponseData creates the response data structure from request information
 func (h *Handler) buildResponseData(req *http.Request, httpReq *HTTPRequest, ipInfo *IPInfo) map[string]interface{} {
-	// Start with basic request data
+	// Start with basic request data - using capitalized keys to match template expectations
 	data := map[string]interface{}{
-		"ip_addr":         httpReq.IP,
-		"user_agent":      req.UserAgent(),
-		"host":            req.Host,
-		"accept":          req.Header.Get("Accept"),
-		"accept_encoding": req.Header.Get("Accept-Encoding"),
-		"accept_language": req.Header.Get("Accept-Language"),
-		"dnt":             req.Header.Get("DNT"),
-		"language":        req.Header.Get("Language"),
-		"referer":         req.Header.Get("Referer"),
-		"method":          req.Method,
-		"mime_type":       req.Header.Get("Content-Type"),
-		"charset":         req.Header.Get("Charset"),
-		"xff":             req.Header.Get("X-Forwarded-For"),
-		"xri":             req.Header.Get("X-Real-IP"),
+		"IPAddr":         httpReq.IP,
+		"UserAgent":      req.UserAgent(),
+		"Host":           req.Host,
+		"Accept":         req.Header.Get("Accept"),
+		"AcceptEncoding": req.Header.Get("Accept-Encoding"),
+		"AcceptLanguage": req.Header.Get("Accept-Language"),
+		"DNT":            req.Header.Get("DNT"),
+		"Language":       req.Header.Get("Language"),
+		"Referer":        req.Header.Get("Referer"),
+		"Method":         req.Method,
+		"MIMEType":       req.Header.Get("Content-Type"),
+		"Charset":        req.Header.Get("Charset"),
+		"XFF":            req.Header.Get("X-Forwarded-For"),
+		"XRI":            req.Header.Get("X-Real-IP"),
+		// Initialize empty values for fields that templates expect
+		"Country":        "",
+		"City":           "",
+		"Org":            "",
+		"Visitor":        "",
 	}
 
 	// Add Cloudflare specific headers
 	if cfCountry := req.Header.Get("Cf-IPcountry"); cfCountry != "" {
-		data["country"] = cfCountry
+		data["Country"] = cfCountry
 	}
 	if cfVisitor := req.Header.Get("Cf-Visitor"); cfVisitor != "" {
-		data["visitor"] = cfVisitor
+		data["Visitor"] = cfVisitor
 	}
 
 	// Override with IP info from geolocation service if available
 	if ipInfo != nil {
 		if ipInfo.Country != "" {
-			data["country"] = ipInfo.Country
+			data["Country"] = ipInfo.Country
 		}
 		if ipInfo.City != "" {
-			data["city"] = ipInfo.City
+			data["City"] = ipInfo.City
 		}
 		if ipInfo.Org != "" {
-			data["org"] = ipInfo.Org
+			data["Org"] = ipInfo.Org
 		}
 	}
 
